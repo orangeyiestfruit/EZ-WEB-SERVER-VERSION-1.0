17d0193cd59fe3aa96dfb4422aa949f1bc804ecf
orangeyiestfruit <paulbrozenec@gmail.com>
2016-03-12 23:19:21 -0800

    Create client.py
    Copy and pasted from IDLE.(FYI)

---
+++ client.py
@@ -0,0 +1,14 @@
+import socket
+import re
+addr = ('localhost',10000)
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+sock.connect(addr)
+alldata = []
+lines = 0
+f = open("main.html","r")
+for line in f:
+    alldata.append(line)
+    lines+=1
+msg = "".join(alldata)
+msg = re.sub('\n', '', msg)
+sock.sendall(msg.encode('utf-8'))
