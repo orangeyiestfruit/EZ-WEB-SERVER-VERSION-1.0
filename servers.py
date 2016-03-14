8458fdd07c3b19bfe929524f3230a9d6d6060569
orangeyiestfruit <paulbrozenec@gmail.com>
2016-03-13 19:40:00 -0700

    Update servers.py
    Now it's actually a server. Now, you connect to the server; not the other way around!

--- servers.py
+++ servers.py
@@ -2,13 +2,17 @@ import socket
 import re
 addr = ('localhost',10000)
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect(addr)
-alldata = []
-lines = 0
-f = open("main.html","r")
-for line in f:
-    alldata.append(line)
-    lines+=1
-msg = "".join(alldata)
-msg = re.sub('\n', '', msg)
-sock.sendall(msg.encode('utf-8'))
+while True:
+    try:
+        sock.connect(addr)
+        alldata = []
+        lines = 0
+        f = open("main.html","r")
+        for line in f:
+            alldata.append(line)
+            lines+=1
+        msg = "".join(alldata)
+        msg = re.sub('\n', '', msg)
+        sock.sendall(msg.encode('utf-8'))
+    except:
+        pass
